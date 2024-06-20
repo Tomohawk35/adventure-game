34291c946ff1d0ee66052eb8156f59b02dd12318
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2024-06-19 22:06:19 -0400

    Added additional monster types

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from functions import create_enemy, fight, battle
 # name / base_health / base_attack_damage / base_experience_bounty
 monster_types = [("Goblin", 30, 5, 10), 
                  ("Harpy", 25, 8, 10), 
-                 ("Orc", 45, 10, 15)]
- 
+                 ("Orc", 45, 10, 15),
+                 ("Dragon", 80, 30, 80),
+                 ("Frog", 10, 2, 2),
+                 ("Zombie", 30, 10, 15)]
+
 def main():
-    print()
-    input_name = input("What is your name? ")
+    input_name = input("\nWhat is your name? ")
     player1 = person(input_name)
     print("\n===== A NEW HERO HAS ARRIVED! ===== \n")
     print(f"Welcome to Evendale, {player1.name}.\n")
